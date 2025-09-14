ee83a08517f289ad873e1481d4068ed363ce5752
Thomas Kuschan <mail@thomaskuschan.de>
2025-09-14 15:54:31 +0200

    Fix build process

--- .build/build.sh
+++ .build/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+app="churchtools_integration"
 tag="$(git describe --tags --abbrev=0)"
 output="../dist/churchtools_integration_$tag.tar.gz"
 cd .build/tmp
@@ -19,5 +20,5 @@ npm run build --no-interaction --no-progress
 
 echo "Start packing"
 rm $output
-tar -czvf $output appinfo css img js l10n lib src templates
+tar --transform "flags=r;s|^|$app/|" -czvf $output appinfo css img js l10n lib src templates vendor
 echo "Output: $output"
