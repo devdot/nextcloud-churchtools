1d9ca1d158462893d3599dd46549f75fd502162b
Thomas Kuschan <mail@thomaskuschan.de>
2026-07-22 13:49:25 +0200

    Version 0.4.0

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -11,7 +11,6 @@ use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
-use OCP\EventDispatcher\IEventDispatcher;
 use OCP\User\Events\PostLoginEvent;
 
 /**
